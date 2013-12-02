34fc2f3bfcd5728de6a91aefadf5cebc5bc3372e
cellosu <cello.su1105@gmail.com>
2013-12-02 17:43:00 +0900

    Update developer.php

--- developer.php
+++ developer.php
@@ -1,21 +1,19 @@
 <?php
 $coin = $_POST['coin'];
 
-        // データベースへの接続
-        try {
-            $dbh = new PDO('mysql:host=localhost;dbname=task;','gacha', 'tasktask');
-        } catch(PDOException $e) {
-            var_dump($e->getMessage());
-            exit;
-        }
-        
+    // データベースへの接続
+    try {
+        $dbh = new PDO('mysql:host=localhost;dbname=task;','gacha', 'tasktask');
+    } catch(PDOException $e) {
+        var_dump($e->getMessage());
+        exit;
+    }
+
         // 処理
-    $sql = "select * from users";
+    $sql = "update users set coin = coin + ".$coin;
     $stmt = $dbh->query($sql);
     foreach($stmt->fetchAll(PDO::FETCH_ASSOC) as $user) {
-        $c = $coin + $user['coin'];
-        $stmt = $dbh->prepare("update users set coin = coin");
-        $stmt->execute(array("coin" >= $c));
+        print $user['coin']."\n";
     }
         // 切断
         $dbh = null;
@@ -24,16 +22,19 @@ $coin = $_POST['coin'];
 
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN">
 <html lang="ja">
-        <head>
-                <title>Developer</title>
-        </head>
-        <body>
-                <h1>所持金付与画面</h1>
-                <p></p>
-                <form action="Developper.php" method="post">
-                    付与するコイン数を入力してください<br>
-                    <input type="text" name="coin" default="0"><br>
-                    <input type="submit" value="ok">
-                </form>
-                <br><br>
-        </body>
+   <head>
+      <meta http-equiv="Content-Type" content="text/html; UTF-8">
+      <meta http-equiv="Content-Style-Type" content="text/css">
+      <link rel="stylesheet" type="text/css" href="style.css">
+      <title>Developer</title>
+   </head>
+   <body>
+       <h2>所持金付与画面</h2>
+      <form action="developer.php" method="post">
+         付与するコイン数を入力してください<br>
+         coin<br>
+         <input type="text" name="coin" default="0"><br>
+         <input type="submit" value="ok">
+      </form>
+   </body>
+</html>
